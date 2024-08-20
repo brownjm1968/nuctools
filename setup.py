8623f67bf90c75371979dbebda5254606b90cea2
brownjm1968 <brownjm@ornl.gov>
2024-08-20 10:51:31 -0400

    update workflows

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup, find_packages
 
-# set the __version__
-exec(open('nuctools/_version.py').read())
 
 setup(
     name='nuctools',
-    version=__version__,
+    version='{{VERSION_PLACEHOLDER}}',
     description="nuclear data reduction tools",
     author="Jesse Brown",
     author_email="brownjm1968@gmail.com",
     packages=['nuctools'],
     include_package_data=True,
     license='BSD 3-clause',
+    url='https://github.com/brownjm1968/nuctools',
     long_description_content_type="text/markdown",
     long_description=open('README.md').read(),
     install_requires=['numpy','pandas','scipy','h5py','pyyaml',
