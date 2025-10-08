8474f53af2ba557c9beeef751567f9138a7d04f4
brownjm1968 <brownjm@ornl.gov>
2025-10-08 10:38:24 -0400

    add file saving function for venus reader

--- nuctools/venus_tools.py
+++ nuctools/venus_tools.py
@@ -2,7 +2,7 @@ import tifffile as tif
 import numpy as np
 import glob
 
-def tif_to_tof(folder,return_mean=False):
+def tif_to_tof(folder,return_mean=False,savefile=None):
     """
     Get sum of counts for each `.tif` file from a VENUS
     experimental campaign and place in order of file name
@@ -13,6 +13,12 @@ def tif_to_tof(folder,return_mean=False):
     ----------
     folder : str
         The path to the folder containing the .tif files
+    return_mean : bool, optional
+        Return the mean instead of sum of counts for a 
+        slice (tof bin).
+    savefile : str, optional
+        The path for the file to write the data to. If the
+        default of None is kept, no file is written
 
     Returns
     -------
@@ -21,9 +27,7 @@ def tif_to_tof(folder,return_mean=False):
         alphabetically sorted file names. Typically the
         file names have a prefix and a number indicating
         "Slice"-number for each time-of-flight bin
-    return_mean : bool, optional
-        Return the mean instead of sum of counts for a 
-        slice (tof bin).
+
     Examples
     --------
     >>> import nuctools as nuc
@@ -42,4 +46,12 @@ def tif_to_tof(folder,return_mean=False):
         else:
             counts[i] = img_array.sum()
     print(f"Read {len(filelist)} files in folder: {folder}")
+
+    if savefile is not None:
+        with open(savefile,'w') as f:
+            f.write('Slice,sum\n')
+            for i,val in enumerate(counts):
+                f.write(f"{i+1},{val:.4f}\n")
     return counts
+
+
